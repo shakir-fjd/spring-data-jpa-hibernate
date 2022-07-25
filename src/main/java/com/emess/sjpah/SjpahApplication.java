6e98efd57248c72b5033951946e3467ecfc07b19
Mohammad Shakir <65658120+Shaquib47@users.noreply.github.com>
2022-07-25 22:37:07 +0530

    initial commit

---
+++ src/main/java/com/emess/sjpah/SjpahApplication.java
@@ -0,0 +1,13 @@
+package com.emess.sjpah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjpahApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SjpahApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emess/sjpah/SjpahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emess.sjpah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjpahApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
